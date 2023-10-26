9007f454649fb2a2f51b3199dc35614722183fd3
Andres Perdomo <136643768+NRDS92@users.noreply.github.com>
2023-10-26 11:49:15 -0500

    Initial commit

---
+++ src/components/Tooltip.svelte
@@ -0,0 +1,28 @@
+<script>
+  export let data;
+  export let xScale;
+  export let yScale;
+
+  console.log(xScale(data.grade));
+</script>
+
+<div class="tooltip" style="position: absolute;
+    top: {yScale(data.hours)}px;
+    left: {xScale(data.grade)}px; ">
+<h1>{data.name}</h1>
+<p>{data.hours} hours Studied</p>
+
+</div>
+<style>
+  .tooltip {
+    padding: 6px;
+    background: withe;
+    border: 1px solid rgba(0, 0, 0, 0.5);
+    pointer-events: none;
+    transition: top 300ms, left 380ms;
+  }
+  h1 {
+    font-weight: 700;
+    margin-bottom: 0.25rem;
+  }
+</style>
